127a494ba81c21f4e3f16d107f1484df43dfb272
Luqmanul Hakim <64834791+lqmaan@users.noreply.github.com>
2023-03-01 21:34:40 +0700

    Update index.js

--- scripts/index.js
+++ scripts/index.js
@@ -4,7 +4,7 @@ const giftbox = document.getElementById('merrywrap');
 const canvasC = document.getElementById('c');
 
 const config = {
-  birthdate: 'Feb 21, 2023',
+  birthdate: 'Feb 21, 2024',
   name: 'Syna Gevariel Alexander'
 };
 
